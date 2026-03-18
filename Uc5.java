16991a58b256142180486d9b5239c806c3d22950
Adrita Chakraborty <ac0666@srmist.edu.in>
2026-03-18 08:33:09 +0530

    UC 5

---
+++ Uc5.java
@@ -0,0 +1,18 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        // Print the result
+        System.out.println("Hello " + nameBuilder.toString());
+    }
+}
